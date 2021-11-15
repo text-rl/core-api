849814c3022e98c93e8cea27ce481231b10968f5
BAILLEUL <bailleul.sacha@gmail.com>
2021-11-15 20:05:33 +0100

    [chore]: Clean log error

--- CoreApi.Client/Messaging/NotifierListenerService.cs
+++ CoreApi.Client/Messaging/NotifierListenerService.cs
@@ -45,8 +45,8 @@ namespace CoreApi.Web.Messaging
             }
             catch (Exception ex)
             {
-                Logger.LogError("Process fail,error:{ExceptionMessage},stackTrace:{ex.StackTrace},message:{Message}",
-                    ex.Message, message);
+                Logger.LogError("Process fail,error:{ExceptionMessage},stackTrace:{StackTrace},message:{Message}",
+                    ex.Message,ex.StackTrace, message);
                 Logger.LogError(-1, ex, "Process fail");
                 return false;
             }
